61455372a3b58abb7b7860229892535683e1240e
Alex-Cas <31858792+Alex-Cas@users.noreply.github.com>
2018-12-01 17:16:22 +0100

    test

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -1,9 +1,9 @@
 import 'should'
 
-const a: number = 0
+const a: number = 1
 
 describe('Metrics', function () {
   it('should save and get', function () {
-    a.should.equal(0)
+    a.should.equal(1)
   })
 })
